fb8c7d8d07aa1f9a4f43911bdb7c2d739d8c9389
tuyenti <tuyen.khac.nguyen@gmail.com>
2017-02-09 02:03:13 +0000

    draw boxes

---
+++ tuyenti/session04/dict_lab.py
@@ -0,0 +1,57 @@
+def dictionary_add(curent_dict, key,value):
+    curent_dict[key] = value
+
+def remove_entry(diction,key,entry):
+    diction[key].remove(entry)
+
+def replacewith(curent_dict, character):
+    for i in curent_dict:
+        for j in range(len(curent_dict[i])):
+            assert isinstance(character, object)
+            curent_dict[i][j] = character
+
+def setbuild(divisible, maximum):
+    i = 0
+    myset = set()
+    while (True):
+        value = i * divisible
+        if (value <= maximum):
+            myset.add(i*divisible)
+            i +=1
+        else:
+            break
+    return myset
+
+def setexercise():
+    s2 = setbuild(2, 20)
+    s3 = setbuild(3, 20)
+    s4 = setbuild(4, 20)
+    print(s2)
+    print(s3)
+    print(s4)
+    print(str(s3.issubset(s2)))
+    print(str(s4.issubset(s2)))
+    pythonset = set('Python')
+    pythonset.add('i')
+    fs = frozenset('marathon')
+    print(pythonset.union(fs))
+    print(pythonset.intersection(fs))
+
+def test_diction():
+    new_dict = dict()
+    dictionary_add(new_dict,'Chris Seattle Chocolate',['name','city','cake'])
+    print(new_dict)
+    remove_entry(new_dict,'Chris Seattle Chocolate','cake')
+    print(new_dict)
+    dictionary_add(new_dict,'Mango',['fruit'])
+    print(new_dict.keys())
+    print(new_dict.values())
+    keyindiction = 'cake' in new_dict
+    print('\'cake\' is they key: ' + str(keyindiction))
+    keyindiction = 'Mango' in new_dict
+    print('\'Mango\' is they key: ' + str(keyindiction))
+    replacewith(new_dict, 't')
+    print(new_dict)
+
+test_diction()
+setexercise()
